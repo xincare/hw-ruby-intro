c120eaf79abeaf44c0dea3b270c3bc48eb64bd26
xincare <cindy_xiao98@yahoo.ca>
2019-09-17 17:26:41 +0000

    finished part I and II

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -15,20 +15,34 @@ end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  arr.permutation(2).to_a.any? {|a,b| a+b==n}
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, " << name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s.empty?
+    return false
+  else
+    /\A+[a-z&&[^aeiou]]/ =~ s.downcase
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s.empty? or /[a-z]/=~s
+    return false
+  elsif s.to_i(2)%4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
